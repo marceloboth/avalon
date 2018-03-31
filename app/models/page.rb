e6d883981b1f98655b77e6629d12ed54f67df2de
Marcelo J. Both <marcelo.both@gmail.com>
2018-03-31 15:00:29 -0300

    Created relation by page and category.

--- app/models/page.rb
+++ app/models/page.rb
@@ -2,6 +2,8 @@ class Page < ApplicationRecord
   include FriendlyId
   friendly_id :title, use: :slugged
 
+  belongs_to :category
+
   validates :title, presence: true
   validates :body, presence: true
 end
---
+++ db/migrate/20180331142758_add_category_to_pages.rb
@@ -0,0 +1,5 @@
+class AddCategoryToPages < ActiveRecord::Migration[5.1]
+  def change
+    add_reference :pages, :category, foreign_key: true
+  end
+end
